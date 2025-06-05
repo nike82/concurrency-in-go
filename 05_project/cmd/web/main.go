173385549f15e0ef328ea8df4c0476e169d04acd
nike <59625573+nike82@users.noreply.github.com>
2025-06-05 20:50:33 +0400

    final project init

---
+++ 05_project/cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// connect to db
+
+	// create sessions
+
+	// create channels
+
+	// create wait group
+
+	// set up the application config
+
+	// set up mail
+
+	// listen for web connections
+
+}
