72ab9d370911744b4712964e8a8b7be3210f2ac0
nike <59625573+nike82@users.noreply.github.com>
2025-06-07 17:36:51 +0400

    send email concurrently

--- 05_project/cmd/web/main.go
+++ 05_project/cmd/web/main.go
@@ -50,6 +50,8 @@ func main() {
 	}
 
 	// set up mail
+	app.Mailer = app.createMail()
+	go app.listenForMail()
 
 	// listen for signals
 	go app.listenForShutdown()
@@ -128,7 +130,7 @@ func openDB(dsn string) (*sql.DB, error) {
 // initSession sets up a session, using Redis for session store
 func initSession() *scs.SessionManager {
 	gob.Register(data.User{})
-	
+
 	// set up session
 	session := scs.New()
 	session.Store = redisstore.New(initRedis())
@@ -165,8 +167,35 @@ func (app *Config) shutdown() {
 	// perform any cleanup tasks
 	app.InfoLog.Println("would run cleanup tasks...")
 
+	
 	// block until waitgroup is empty
 	app.Wait.Wait()
+	app.Mailer.DoneChan <- true
 
 	app.InfoLog.Println("closing channels and shutting down application...")
+	close(app.Mailer.MailerChan)
+	close(app.Mailer.ErrorChan)
+	close(app.Mailer.DoneChan)
+}
+
+func (app *Config) createMail() Mail {
+	// create channels
+	errorChan := make(chan error)
+	mailerChan := make(chan Message, 100)
+	mailerDoneChan := make(chan bool)
+
+	m := Mail{
+		Domain: "localhost",
+		Host: "localhost",
+		Port: 1025,
+		Encryption: "none",
+		FromName: "Info",
+		FromAddress: "info@mail.com",
+		Wait: app.Wait,
+		ErrorChan: errorChan,
+		MailerChan: mailerChan,
+		DoneChan: mailerDoneChan,
+	}
+
+	return m
 }
